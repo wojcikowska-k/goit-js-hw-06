d107f97f50bbfeac07566465b31e36a57973077b
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-12 16:49:31 +0100

    task #2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,27 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// ###
+
+const list = document.getElementById("ingredients");
+
+// const markup = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
+
+// list.innerHTML = markup;
+
+// #########
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const element = document.createElement("li");
+  element.textContent = ingredients[i];
+  element.classList.add("item");
+  list.append(element);
+}
