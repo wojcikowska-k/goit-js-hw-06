d73966badb3e989b47ca8a7893e4a3975b95d932
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-15 23:10:50 +0100

    task #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(`.login-form`);
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
