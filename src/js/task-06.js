c60aac2ee7589d54b16a562f3aa13740b411ea78
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-15 19:19:27 +0100

    task #6

---
+++ src/js/task-06.js
@@ -0,0 +1,13 @@
+// const input = document.getElementById(`validation-input`);
+const input = document.querySelector(`input`);
+
+const checkLength = () => {
+  if (input.textLength < 6) {
+    input.classList.add(`invalid`);
+  } else {
+    input.classList.remove(`invalid`);
+    input.classList.add(`valid`);
+  }
+};
+
+input.addEventListener("blur", checkLength);
