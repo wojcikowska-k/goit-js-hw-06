53aba0eb02873dd784212e7d52bce487ff45a29d
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-16 14:00:19 +0100

    task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const span = document.querySelector(".color");
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeColor() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  span.innerHTML = `${getRandomHexColor()}`;
+}
+
+btn.addEventListener("click", changeColor);
