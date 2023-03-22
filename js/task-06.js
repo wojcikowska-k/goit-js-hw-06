3b42d3a0d10b7a30135700cf4402e4ebc6cd178c
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-22 14:05:21 +0100

    changed task#6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const input = document.querySelector(`input`);
 
 const checkLength = () => {
-  if (input.textLength < Number(input.dataset.length)) {
+  if (input.value.length < Number(input.dataset.length)) {
     input.classList.remove(`valid`);
     input.classList.add(`invalid`);
   } else {
